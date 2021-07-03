da0937cea472d2f4416bb5f90dd4fcaf6d17ac54
Ray Hurst <rhurst1125@berkeley.edu>
2021-07-02 20:39:36 -0700

    neated up extra comments from starter code

--- Workspace/test_qualifier.py
+++ Workspace/test_qualifier.py
@@ -14,8 +14,7 @@ from loan_modules.qualifier.filters import loan_to_value
 from loan_modules.qualifier.filters import max_loan_size
 
 
-# #     # @TODO: Your code here!
-# #     # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
+# Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
 
 def test_calculate_monthly_debt_ratio():
     assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
@@ -35,7 +34,9 @@ def test_filters():
 
     loan_to_value_ratio = 0.84
 
-# @TODO: Test the new save_csv code!
+# Test the new save_csv code!
 def test_save_csv():
-    user_save_path_ouput = fileio.save_csv(Path('test_output_path.csv'), qualifying_loans = ["Test", "Testy"])
+    user_save_path_ouput = fileio.save_csv(Path('loan_modules/data/output/qualifying_loans.csv'), qualifying_loans = ["Test", "Testing"])
 # have confirmed save_csv is writing
+
+test_save_csv()
