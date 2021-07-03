10fb0779750fccfda15e99cc51ec28520499fc6c
Ray Hurst <rhurst1125@berkeley.edu>
2021-07-02 19:51:37 -0700

    removed redudant libraries from fileio

--- Workspace/loan_modules/qualifier/utils/fileio.py
+++ Workspace/loan_modules/qualifier/utils/fileio.py
@@ -5,10 +5,10 @@ This contains a helper function for loading and saving CSV files.
 
 """
 import csv
-import questionary
 
 
 def load_csv(csvpath):
+
     """Reads the CSV file from path provided.
 
     Args:
@@ -33,6 +33,15 @@ def load_csv(csvpath):
 # creating initial save_csv() function
 
 def save_csv(user_save_path_ouput, qualifying_loans):
+
+    """Writes to a csv file
+
+    Args: 
+    user_save_path_ouput: the desired output path for the new csv content
+    qualifying_loans: the data to be written into the csv file
+
+    """
+
     #qual_loanscsv_output_path = questionary.text("Please the desired path for your new saved csvfile").ask()
     #currently written to intake output path from save_qualifying_loans()
     with open (user_save_path_ouput, 'w', newline='') as user_qual_loans_csvfile:
